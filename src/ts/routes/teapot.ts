0ed3d8bc07c0b55557fd2f523df510e400e1ed10
jly02 <lyjonathan02@gmail.com>
2022-08-10 11:00:01 -0700

    Rename some stuff

--- src/ts/routes/teapot.ts
+++ src/ts/routes/teapot.ts
@@ -10,21 +10,21 @@ const router = express.Router();
  * I'm a teapot.
  */
 router.get('/418/', (req, res) => { 
-    res.status(418).sendFile('src/html/teapot.html', {root: path.dirname(path.dirname(__dirname))});
+    res.status(418).sendFile('src/pages/teapot.html', {root: path.dirname(path.dirname(__dirname))});
 });
 
 /**
  * Teapot styling.
  */
 router.get('/assets/teapot.css', (req, res) => {
-    res.status(200).sendFile('src/assets/css/teapot.css', {root: path.dirname(path.dirname(__dirname))});
+    res.status(200).sendFile('src/pages/assets/css/teapot.css', {root: path.dirname(path.dirname(__dirname))});
 });
 
 /**
  * Teapot picture.
  */
 router.get('/assets/teapot.png', (req, res) => {
-    res.status(200).sendFile('src/assets/img/teapot.png', {root: path.dirname(path.dirname(__dirname))});
+    res.status(200).sendFile('src/pages/assets/img/teapot.png', {root: path.dirname(path.dirname(__dirname))});
 });
 
 export default router;
