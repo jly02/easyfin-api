51565e279e5ce0ff49a2ed2836d7bff0b2bf5aac
jly02 <lyjonathan02@gmail.com>
2022-08-17 11:18:58 -0700

    Implement add-stock and remove-stock

--- src/ts/routes/users.ts
+++ src/ts/routes/users.ts
@@ -11,6 +11,10 @@ import { query } from './lib/queries';
  */
 const router = express.Router();
 
+/**
+ * Results in a JSON string containing all of the stocks a particular user has,
+ * and their quantities.
+ */
 router.get('/get-stocks/', async (req, res) => {
     const { username }: { username: string } = req.body;
 
@@ -56,8 +60,100 @@ router.get('/get-stocks/', async (req, res) => {
     }
 });
 
-router.post('/add-stock/', (req, res) => {
+/**
+ * Handles adding a stock to a user's account.
+ */
+router.post('/add-stock/', async (req, res) => {
+    const { username, symbol, amount }: { username: string, symbol: string, amount: number } = req.body;
 
+    // Look for 'Authorization' header, which holds a user's unique API key.
+    let key: string;
+    try {
+        key = req.header('Authorization');
+    } catch(error) {
+        // HTTP - 401 Unauthorized
+        res.status(401).send({
+            error,
+            valid: false,
+            message: "missing header token"
+        }).end();
+
+        return;
+    }
+
+    // Look for user in database
+    let id: UserId[] = await query(`SELECT user_id FROM users WHERE user_name = '${username}'`);
+    if(!id[0]) {
+        res.status(404).send({
+            valid: false,
+            message: "User not found!"
+        }).end();
+
+        return;
+    }
+
+    // Validate API key
+    let valid: boolean = await validate(id[0].user_id, key);
+    if(!valid) {
+        res.status(401).send({
+            message: "Could not validate key!"
+        }).end();
+    } else {
+        let response = await query(`INSERT INTO stocks VALUES('${id}', '${symbol}', ${amount})`);
+
+        res.status(200).send({
+            response,
+            message: "Successfully added data!"
+        });
+    }
+});
+
+/**
+ * Removes a stock from a user's account.
+ */
+router.delete('/remove-stock/', async (req, res) => {
+    const { username, symbol }: { username: string, symbol: string } = req.body;
+
+    // Look for 'Authorization' header, which holds a user's unique API key.
+    let key: string;
+    try {
+        key = req.header('Authorization');
+    } catch(error) {
+        // HTTP - 401 Unauthorized
+        res.status(401).send({
+            error,
+            valid: false,
+            message: "missing header token"
+        }).end();
+
+        return;
+    }
+
+    // Look for user in database
+    let id: UserId[] = await query(`SELECT user_id FROM users WHERE user_name = '${username}'`);
+    if(!id[0]) {
+        res.status(404).send({
+            valid: false,
+            message: "User not found!"
+        }).end();
+
+        return;
+    }
+
+    // Validate API key
+    let valid: boolean = await validate(id[0].user_id, key);
+    if(!valid) {
+        res.status(401).send({
+            message: "Could not validate key!"
+        }).end();
+    } else {
+        let response = await query(`DELETE FROM stocks WHERE user_id = ${id} AND symbol = '${symbol}'`);
+
+        res.status(200).send({
+            response,
+            message: "Successfully removed data!"
+        });
+    }
 });
 
 export default router;
